92a323b4154c86d30f4e20553c685408d9b5e642
jorgeimbiriba <jorge.henrique7171@gmail.com>
2025-07-02 08:31:39 -0400

    Mudança da função paste

--- QuestoesED.R
+++ QuestoesED.R
@@ -65,13 +65,14 @@ umidade <- seq(from = 0.4, to = 1, by = 0.1) # O mais correto, funciona como a s
 matriz3 <- matrix(c(1:1000),nrow = 100, ncol = 10)
 
 # Criando os nomes das linhas
-nomes_linhas <- paste0("Linha ", 1:100)
+nomes_linhas <- paste("Linha ", 1:100)
 
 # Atribuindo os nomes à matriz
 rownames(matriz3) <- nomes_linhas
 
 # Criando os nomes das colunas
-nomes_colunas <- paste0("Coluna ", 1:10)
+nomes_colunas <- paste("Coluna ", 1:10)
 
 # Atribuindo os nomes à matriz
 colnames(matriz3) <- nomes_colunas
+
